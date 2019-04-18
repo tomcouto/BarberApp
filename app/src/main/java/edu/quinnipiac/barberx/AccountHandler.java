1f0e70f652423c162cf1f1a5c77f6dcf95dd03be
Tom Couto <tacouto@quinnipiac.edu>
2019-04-18 11:36:40 -0400

    added a handler to keep local information

---
+++ app/src/main/java/edu/quinnipiac/barberx/AccountHandler.java
@@ -0,0 +1,33 @@
+package edu.quinnipiac.barberx;
+
+import java.util.ArrayList;
+
+public class AccountHandler {
+
+    /**
+     * This class will add all the account details to an array
+     * then allow the app local access to the database after login
+     * to avoid reaching the database to get info every time
+     */
+
+    //local account details
+    ArrayList<String> accountDetails = new ArrayList<>();
+
+    //returns the local username
+    public String getUsername(){
+        return null;
+    }
+
+    //return local email
+    public String getEmail() {
+        return null;
+    }
+
+    //return profile picture
+
+    //return all requests
+
+    //return schedule
+
+    //return all uploaded images for profile page
+}
