c13fc63ae2bd667bf0dbced3a0f83d80f061fac5
Tom Couto <tacouto@quinnipiac.edu>
2019-04-18 15:06:51 -0400

    Added comments for class descriptions

--- app/src/main/java/edu/quinnipiac/barberx/ProfileFragment.java
+++ app/src/main/java/edu/quinnipiac/barberx/ProfileFragment.java
@@ -1,12 +1,18 @@
 package edu.quinnipiac.barberx;
 
+/**
+ * ProfileFragment shows the barber's profile filled with personal data saved to the database.
+ *
+ * Version: 1.0
+ * Authors: Tom Couto and Dominic Smorra
+ */
+
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 
 public class ProfileFragment extends Fragment {
