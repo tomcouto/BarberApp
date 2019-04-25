00220ee36111d501e385771c5f02769eccff88d2
Dominic Smorra <dominicsmorra@gmail.com>
2019-04-25 13:04:13 -0400

    beginning of tests

---
+++ app/src/androidTest/java/edu/quinnipiac/barberx/LoginActivityTest.java
@@ -0,0 +1,24 @@
+package edu.quinnipiac.barberx;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+}
---
+++ app/src/androidTest/java/edu/quinnipiac/barberx/ProfileFragmentTest.java
@@ -0,0 +1,32 @@
+package edu.quinnipiac.barberx;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProfileFragmentTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void newInstance() {
+
+    }
+
+    @Test
+    public void onCreateView() {
+
+    }
+}
